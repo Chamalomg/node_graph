3e9654c6755cfd58cf947db95b6098b13204e97f
Fabien SOLDE <fabien.solde@epfedu.fr>
2021-02-15 21:54:34 +0100

    add color

--- Champ.py
+++ Champ.py
@@ -1,11 +1,19 @@
 class Champ:
+    all_traits = [
+        'Boss', 'Cultist', 'Divine', 'Duelist', 'Emperor', 'Fortune', 'Keeper', 'Set4_Adept', 'Set4_Assassin',
+        'Set4_Blacksmith', 'Set4_Brawler', 'Set4_Daredevil', 'Set4_Dragonsoul', 'Set4_Elderwood',
+        'Set4_Enlightened', 'Set4_Executioner', 'Set4_Exile', 'Set4_Fabled', 'Set4_Mage', 'Set4_Mystic',
+        'Set4_Ninja', 'Set4_Slayer', 'Set4_Spirit', 'Set4_Syphoner', 'Set4_Vanguard', 'Sharpshooter',
+        'Warlord'
+    ]
+
+    traits_color = {key: i / 27 for i, key in enumerate(all_traits)}
 
     def __init__(self, name, cost, traits):
         self.name = name
         self.cost = cost
         self.traits = traits
+        self.color = self.traits_color[traits[0]]
 
     def __str__(self):
         return '{}'.format(self.name)
-
-
--- main.py
+++ main.py
@@ -16,6 +16,13 @@ nodes = []
 
 edges = []
 
+traits = ['Boss', 'Cultist', 'Divine', 'Duelist', 'Emperor', 'Fortune', 'Keeper', 'Set4_Adept', 'Set4_Assassin',
+          'Set4_Blacksmith', 'Set4_Brawler', 'Set4_Daredevil', 'Set4_Dragonsoul', 'Set4_Elderwood',
+          'Set4_Enlightened', 'Set4_Executioner', 'Set4_Exile', 'Set4_Fabled', 'Set4_Mage', 'Set4_Mystic',
+          'Set4_Ninja', 'Set4_Slayer', 'Set4_Spirit', 'Set4_Syphoner', 'Set4_Vanguard', 'Sharpshooter', 'Warlord']
+
+traits_color = {key: i/27 for i, key in enumerate(traits)}
+
 
 def add_nodes_json(path):
     with open(path) as champs:
@@ -30,7 +37,7 @@ def add_edges(nodes):
         for j in range(n):
             if all([i != j, nodes[i].traits, (nodes[i], nodes[j]) not in edges]):
                 L = [nodes[i].traits[k] == nodes[j].traits[l]
-                       for k in range(len(nodes[i].traits)) for l in range(len(nodes[j].traits))]
+                     for k in range(len(nodes[i].traits)) for l in range(len(nodes[j].traits))]
                 if any(L):
                     edges.append((nodes[i], nodes[j]))
 
@@ -38,7 +45,9 @@ def add_edges(nodes):
 def plot_graph(nodes, edges):
     G = nx.Graph()
     G.add_nodes_from(nodes), G.add_edges_from(edges)
-    nx.draw(G, with_labels=True, font_weight='bold')
+    node_color = [node.color for node in G.nodes()]
+    print(node_color)
+    nx.draw(G, cmap=plt.get_cmap('viridis'), node_color=node_color, with_labels=True, font_weight='bold')
     plt.show()
 
 
@@ -46,4 +55,3 @@ if __name__ == '__main__':
     add_nodes_json(PATH)
     add_edges(nodes)
     plot_graph(nodes, edges)
-    a = 1
